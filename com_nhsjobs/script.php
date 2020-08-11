9b5f217d07b5bedf83173ec11db611edc80e4189
Kiran Cheema <kiran.cheema@nhs.net>
2020-08-11 16:30:24 +0100

    Add files via upload

---
+++ com_nhsjobs/script.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @package    COM_NHSJOBS
+ *
+ * @author     NHS South, Central and West <webteam.scwcsu@nhs.net>
+ * @copyright  Copyright (C) 2019 NHS South Central and West. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       https://www.scwcsu.nhs.uk
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Nhsjobs script file.
+ *
+ * @package   COM_NHSJOBS
+ * @since     1.0.0
+ */
+class Com_NhsjobsInstallerScript
+{
+	/**
+	 * Constructor
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 */
+	public function __construct(JAdapterInstance $adapter) {}
+
+	/**
+	 * Called before any type of action
+	 *
+	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function preflight($route, JAdapterInstance $adapter) {}
+
+	/**
+	 * Called after any type of action
+	 *
+	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function postflight($route, JAdapterInstance $adapter) {}
+
+	/**
+	 * Called on installation
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function install(JAdapterInstance $adapter) {}
+
+	/**
+	 * Called on update
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 *
+	 * @return  boolean  True on success
+	 */
+	public function update(JAdapterInstance $adapter) {}
+
+	/**
+	 * Called on uninstallation
+	 *
+	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
+	 */
+	public function uninstall(JAdapterInstance $adapter) {}
+}
+
